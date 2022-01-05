4f36c2c528d3ac93cd2789d47b74948df4192af3
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-01-05 16:30:23 +0100

    udpated and rendered emails

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+// #region 'IMPORTING COMPONENTS AND STUFF'
 import Header from './components/Header'
 import initialEmails from './data/emails'
+import { useState } from 'react'
 import './App.css'
+// #endregion
 
 function App() {
 
   // Use initialEmails for state
   console.log(initialEmails)
 
+  // #region 'STATE OBJECT'
+  const [showRead, setShowRead] = useState(false)
+  const [emails, setEmails] = useState(initialEmails)
+  // #endregion
+
+  console.log(emails)
+  let emailsToDisplay = emails //this is key to render emails gets array from state
+
+// #region 'RETURNING AND CREATING HTML SPITTING HTML'
   return (
     <div className="app">
   
@@ -47,15 +59,27 @@ function App() {
       </nav>
 
       <main className="emails">
-        {
-        /* Render a list of emails here */
-        
-        }
+
+        <ul className='emails'>
+
+          {
+            emailsToDisplay.map(email => (
+              <li className="email">
+                <input type="checkbox" checked={false} className='box-checkbox'/>
+                <input type="checkbox" checked={false} className='star-checkbox'/>
+                <span className=''>{email.sender}{' '}</span>
+                <span className='title'>{email.title}{' '}</span>
+              </li>
+          ))}
+
+        </ul>
+
       </main>
 
     </div>
   )
 
 }
+// #endregion
 
 export default App
